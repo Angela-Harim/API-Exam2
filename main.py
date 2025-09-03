7e0fa47109c0264b67eda7d403cd775c0a5eecf3
Angela-Haim <hei.angela.5@gmail.com>
2025-09-03 11:36:26 +0300

    Creation of first route (GET /ping)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def lire_racine():
-    return {"message": "Bienvenue dans mon projet FastAPI "}
+@app.get("/ping")
+def ping():
+    return {"pong"}
+
